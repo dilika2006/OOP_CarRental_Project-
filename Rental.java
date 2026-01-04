359f0848ea14971d0e64470e5cc4bc136a01a0d8
Oumou Diallo <od20326@gmail.com>
2026-01-04 12:27:11 +0300

    Resolve issues for users input

--- Rental.java
+++ Rental.java
@@ -35,6 +35,7 @@ class Rental {
     public int getPaymentId() { return paymentId; }
     public void setPaymentId(int paymentId){this.paymentId = paymentId;}
     public String getStatus() { return status; }
+    public void setStatus(String status){this.status = status;}
 }
 
 
